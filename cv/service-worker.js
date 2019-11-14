2ff32ccfbf5de06df7ae0b07eb3d193d862ed2b5
Rabindra Joshi <mail@rjoshi.net>
2019-11-14 10:11:45 +0545

    PWA because why not? :blush:

---
+++ cv/service-worker.js
@@ -0,0 +1,38 @@
+let deferredPrompt;
+
+const CACHE_VERSION = 1
+const cacheName = `cache-v${CACHE_VERSION}`;
+const precacheResources = [
+  '/',
+  'index.html',
+  'style.css',
+  'script.js',
+  // 'images/*'
+];
+
+self.addEventListener('install', event => {
+  console.log('Service worker install event!');
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(precacheResources);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
